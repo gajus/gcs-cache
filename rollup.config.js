c337265e5b3e38380d7ea7ac3ea773be60267679
Gajus Kuizinas <gajus@gajus.com>
2022-05-11 19:31:28 -0500

    fix: correct bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,12 @@
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
 
 export default {
   input: 'src/main.ts',
   output: {
     format: 'cjs'
   },
-  plugins: [typescript(),commonjs()]
+  plugins: [typescript(),nodeResolve(),json(),commonjs()]
 };
